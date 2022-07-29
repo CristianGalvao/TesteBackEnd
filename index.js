56a0f0c314afbadadbf4850ed2a25ad23eae9068
CristianGalvao <cristian2018galvao@outlook.com>
2022-07-29 13:18:02 -0300

    Teste

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('<span style="color: black">Hello World</span>');
+    console.log("teste")
+});
+
+const PORT = process.env.PORT || 5000;
+
+// CRIANDO O SERVIDOR PARA RODAR NA PORTA 3000
+app.listen(PORT, ()=>{
+    console.log("SERVIDO RODANDO" + PORT);
+})
